df5bb230b184ac29e1c069ade431dacde6ef7674
APH Computer Lab 6 <aphcomputerlab6@APH-Computer-Lab6.local>
2020-12-07 17:00:36 -0500

    working encoders!

--- adamNewt2020/src/main.cpp
+++ adamNewt2020/src/main.cpp
@@ -4,50 +4,90 @@
 #include <Arduino.h>
 #include "RoboClaw.h"
 
-RoboClaw roboclaw(&Serial,10000);
+RoboClaw roboclaw(&Serial1,10000);
 
 #define address 0x80
 
-//Display Encoder and Speed for Motor 1
+//Velocity PID coefficients.
+#define Kp 1.0
+#define Ki 0.5
+#define Kd 0.25
+#define qpps 44000
+
+void setup() {
+  //Open Serial and roboclaw serial ports
+  Serial.begin(115200);
+  roboclaw.begin(38400);
+  
+  //Set PID Coefficients
+  roboclaw.SetM1VelocityPID(address,Kd,Kp,Ki,qpps);
+  roboclaw.SetM2VelocityPID(address,Kd,Kp,Ki,qpps);  
+}
+
 void displayspeed(void)
 {
-  uint8_t status1,status2;
-  bool valid1,valid2;
-  // int32_t enc1 = roboclaw.ReadEncM1(address, &status1, &valid1);
-  // int32_t speed1 = roboclaw.ReadSpeedM1(address, &status2, &valid2);
+  uint8_t status1,status2,status3,status4;
+  bool valid1,valid2,valid3,valid4;
   
+  int32_t enc1= roboclaw.ReadEncM1(address, &status1, &valid1);
+  int32_t enc2 = roboclaw.ReadEncM2(address, &status2, &valid2);
+  int32_t speed1 = roboclaw.ReadSpeedM1(address, &status3, &valid3);
+  int32_t speed2 = roboclaw.ReadSpeedM2(address, &status4, &valid4);
+  Serial.print("Encoder1:");
   if(valid1){
-    //Serial.print("Encoder1:");
-    // Serial.print(enc1,DEC);
-    // Serial.print(" ");
-    // Serial.print(status1,HEX);
-    // Serial.print(" ");
+    Serial.print(enc1,HEX);
+    Serial.print(" ");
+    Serial.print(status1,HEX);
+    Serial.print(" ");
   }
+  else{
+    Serial.print("invalid ");
+  }
+  Serial.print("Encoder2:");
   if(valid2){
-    //Serial.print("Speed1:");
-    // Serial.print(speed1,DEC);
-    // Serial.print(" ");
+    Serial.print(enc2,HEX);
+    Serial.print(" ");
+    Serial.print(status2,HEX);
+    Serial.print(" ");
   }
-  
+  else{
+    Serial.print("invalid ");
+  }
+  Serial.print("Speed1:");
+  if(valid3){
+    Serial.print(speed1,HEX);
+    Serial.print(" ");
+  }
+  else{
+    Serial.print("invalid ");
+  }
+  Serial.print("Speed2:");
+  if(valid4){
+    Serial.print(speed2,HEX);
+    Serial.print(" ");
+  }
+  else{
+    Serial.print("invalid ");
+  }
   Serial.println();
 }
 
-//This is the first function arduino runs on reset/power up
-void setup() {
-  //Open Serial and roboclaw at 38400bps
-  //roboclaw.begin(38400);
-  Serial.begin(9600);
-  Serial.println("Starting...");
-}
-
 void loop() {
-  // roboclaw.SpeedAccelDeccelPositionM1(address,0,12000,0,11000,1);
-  // roboclaw.SpeedAccelDeccelPositionM1(address,0,12000,0,1000,0);
-  // roboclaw.SpeedAccelDeccelPositionM1(address,32000,12000,32000,11000,0);
-  // roboclaw.SpeedAccelDeccelPositionM1(address,32000,12000,32000,1000,0);
-  long last = millis();
-  while(millis()-last<5000){
-    displayspeed();
-    delay(50);
-  }
+  roboclaw.SpeedM1(address,400);
+  roboclaw.SpeedM2(address,-400);
+  Serial.println("Forward");
+  delay(10000);
+  // for(uint8_t i = 0;i<400;i++){
+  //   //displayspeed();
+  //   delay(10);
+  // }
+
+  roboclaw.SpeedM1(address,-400);
+  roboclaw.SpeedM2(address,400);
+  Serial.println("Reverse");
+  // for(uint8_t i = 0;i<400;i++){
+  //   //displayspeed();
+  //   delay(10);
+  // }
+  delay(10000);
 }
